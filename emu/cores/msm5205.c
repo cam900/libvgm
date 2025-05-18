1d4981c7da22ea99942a341e34539b9fab5a7802
cam900 <dbtlrchl@naver.com>
2025-05-18 21:53:41 +0900

    Fix MSM6585 prescaler

--- emu/cores/msm5205.c
+++ emu/cores/msm5205.c
@@ -196,9 +196,15 @@ static void compute_tables(void) {
 }
 
 INLINE UINT32 get_prescaler(msm5205_state *info) {
-    return (info->data_in_last & PIN_S1) ? 
-        ((info->data_in_last & PIN_S2) ? 0 : 64) : 
-        ((info->data_in_last & PIN_S2) ? 48 : 96);
+    if (info->is_msm6585) {
+        return (info->data_in_last & PIN_S1) ? 
+            ((info->data_in_last & PIN_S2) ? 20 : 80) : 
+            ((info->data_in_last & PIN_S2) ? 40 : 160);
+    } else {
+        return (info->data_in_last & PIN_S1) ? 
+            ((info->data_in_last & PIN_S2) ? 1/* Slave mode */ : 64) : 
+            ((info->data_in_last & PIN_S2) ? 48 : 96);
+    }
 }
 
 // ========== Core ADPCM Processing ==========
